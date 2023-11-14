9ab98f85e274c0b8d9974d8086c289fa9b7662bc
Steve Purcell <steve@sanityinc.com>
2023-11-14 11:38:56 +0000

    Flake updates

--- flake.nix
+++ flake.nix
@@ -4,19 +4,22 @@
   inputs =
     {
       nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-      flake-utils.url = "github:numtide/flake-utils";
     };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { localSystem = system; };
-        app = (pkgs.callPackage ./. { });
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [ app.ruby pkgs.bundix app ];
-        };
-      }
-    );
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
+    in
+    {
+      devShell = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs { localSystem = system; };
+            app = (pkgs.callPackage ./. { });
+          in
+          pkgs.mkShell {
+            buildInputs = [ app.ruby pkgs.bundix app ];
+          }
+        );
+    };
 }
